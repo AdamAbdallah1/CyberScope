2cc34b3729631f456ce0daeb02f89f4c0a85df73
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-11 22:03:11 +0300

    Initializing header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <img src={Logo} width={100} alt="CyberScope Logo" />
+    </header>
+  )
+}
+
+export default Header
