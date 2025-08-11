69551587cdc9281352a84dcfaf45613733a168eb
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-11 22:01:50 +0300

    Initializing footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="w-full text-center py-4 text-[#A7FF18]">
+        <p className="text-xs opacity-80 flex justify-center items-center gap-2">
+          <span className="text-red-400">&copy;</span> 2025 CyberScope. All rights reserved.
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
