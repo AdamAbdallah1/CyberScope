55cd14106efe39b234460b3636dce43f43e47a41
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-12 23:47:14 +0300

    Wrapping all components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Scan from './components/Scan';
+import Login from './components/Auth/Login';
 
 const App = () => {
   return (
     <main className='flex flex-col mt-10'>
+      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div>
       <Header />
       <Scan />
       <Footer />
