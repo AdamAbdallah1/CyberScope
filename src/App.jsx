6a033eeaa24963fe1fba84288031c986819e46be
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-13 17:09:28 +0300

    Adding background from bg.ibelick.com

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import Login from './components/Auth/Login';
 
 const App = () => {
   return (
-    <main className='flex flex-col mt-10'>
-      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div>
-      <Header />
-      <Scan />
-      <Footer />
-    </main>
+     <div className='min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='fixed top-0 -z-10 h-full w-full'>
+      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div></div>
+      <div className='container mx-auto px-8 '>
+        <Header />
+        <Scan />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
