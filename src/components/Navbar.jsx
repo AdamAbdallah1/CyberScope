f6a70d8dc560e19af8cd9586133992227aba9854
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-09-29 00:02:24 +0300

    Removing nav menu till now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,32 +39,8 @@ const Navbar = () => {
         width={100}
       />
 
-      <button
-        className="absolute right-4 md:hidden"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <IoIosMenu size={28} className="text-[#A7FF18]" />
-      </button>
-
-      <nav
-        className={`absolute top-full right-0 mt-2 w-48 border border-gray-700 rounded-md shadow-lg md:hidden transition-all duration-300 ${
-          menuOpen ? "block" : "hidden"
-        }`}
-      >
-        <ul className="flex flex-col p-4 gap-3">
-          {userEmail && (
-            <li className="text-[#A7FF18] truncate">{userEmail}</li>
-          )}
-          <li>
-            <button
-              onClick={handleLogout}
-              className="w-full text-[#A7FF18] hover:text-white"
-            >
-              Logout
-            </button>
-          </li>
-        </ul>
-      </nav>
+      
+          
     </header>
   );
 };
