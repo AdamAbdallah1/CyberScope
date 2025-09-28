5c0b5264641980d4802a2469d52e800f54d05348
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-09-28 22:54:07 +0300

    Importing true focus on heading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { div } from 'motion/react-client'
+import TrueFocus from './TrueFocus'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Navbar />
+
+        <div className='w-1/2 flex flex-col gap-10 justify-center items-center m-auto p-10 mt-15'>
+            <TrueFocus 
+                sentence="Cyber Scope"
+                manualMode={false}
+                blurAmount={5}
+                borderColor="#73B117"
+                animationDuration={2}
+                pauseBetweenAnimations={1}
+                />
+            <p>
+                Your all-in-one cybersecurity and digital intelligence platform.  
+                Analyze threats, monitor vulnerabilities, and secure your digital presence with ease.  
+            </p>
+            
+        </div>
+    </div>
   )
 }
 
